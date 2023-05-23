b550b2fc06400fe01e0f0911531690b8090202e7
Crowdbotics <team@crowdbotics.com>
2023-05-23 01:26:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-feather-40911",
+  url: "https://flat-feather-40911.botics.co",
+  api: "https://flat-feather-40911.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
